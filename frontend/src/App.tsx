af4cf2f5af097d6c91ad7ddaa02927d6f44a4496
Your Name <padinmichael201@gmail.com>
2022-08-22 08:54:48 +0800

    Added react router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,22 @@
-import Banner from "./components/Banner";
-import Navbar from "./components/Navbar";
-import Tables from "./components/Tables";
-import {  StateContextProvider } from './context/StateContext';
+import { Banner, Navbar, Tables } from "./components";
+import {AllLoans, BadCreditLoans, PersonalLoans} from "./pages"
+import { StateContextProvider } from "./context/StateContext";
+import {Routes, Route} from "react-router-dom"
 import "./styles/global.css";
 
 function App() {
   return (
-    <StateContextProvider >
-    <div className="App">
-      <nav className="w-full fixed h-[70px] border-b-2 z-20 bg-white">
-        <Navbar />
-      </nav>
-      <section>
-        <Banner />
-      </section>
-      <section>
-        <Tables />
-      </section>
-    </div>
+    <StateContextProvider>
+      <div className="App">
+        <nav className="w-full fixed h-[70px] border-b-2 z-20 bg-white">
+          <Navbar />
+        </nav>
+        <Routes>
+          <Route path = "/" element = {<AllLoans/>}/>
+          <Route path = "/personal-loans" element = {<PersonalLoans/>}/>
+          <Route path = "/bad-credit-loans" element = {<BadCreditLoans/>}/>
+        </Routes>
+      </div>
     </StateContextProvider>
   );
 }
